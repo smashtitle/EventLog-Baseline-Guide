a4f37363be5dae6c33d2c32178d9549b4ce59bab
smashtitle <smashtitle@fastmail.com>
2025-07-04 15:51:33 -0700

    Update and rename ASD.bat to ASD-Servers.bat

--- bat/ASD-Servers.bat
+++ bat/ASD-Servers.bat
@@ -3,10 +3,14 @@ wevtutil sl System /ms:67108864
 wevtutil sl Application /ms:67108864
 
 :: Enable PowerShell Module logging
+wevtutil set-log "Microsoft-Windows-PowerShell/Operational" /enabled:true
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging" /v EnableModuleLogging /t REG_DWORD /d 1 /f
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging\ModuleNames" /v 1 /t REG_SZ /d * /f
 reg add HKLM\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\PowerShell\ModuleLogging /v EnableModuleLogging /f /t REG_DWORD /d 1
 reg add HKLM\SOFTWARE\Wow6432Node\Policies\Microsoft\Windows\PowerShell\ModuleLogging\ModuleNames  /f /v ^* /t REG_SZ /d ^*
 
 :: Enable PowerShell Script Block logging
+reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging /v EnableScriptBlockLogging /t REG_DWORD /d 1 /f
 reg add HKLM\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging /v EnableScriptBlockLogging /f /t REG_DWORD /d 1
 
 ::
@@ -15,17 +19,17 @@ reg add HKLM\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows\PowerShell\ScriptBl
 
 :: Account Logon
 :::: Credential Validation
-:: auditpol /set /subcategory:{0CCE923F-69AE-11D9-BED3-505054503030} /success:enable /failure:disable
+auditpol /set /subcategory:{0CCE923F-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Kerberos Authentication Service (disable for clients)
-:: auditpol /set /subcategory:{0CCE9242-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE9242-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Kerberos Service Ticket Operations (disable for clients)
-:: auditpol /set /subcategory:{0CCE9240-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE9240-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 
 :: Account Management
 :::: Computer Account Management
 auditpol /set /subcategory:{0CCE9236-69AE-11D9-BED3-505054503030} /success:enable /failure:disable
 :::: Other Account Management Events
-auditpol /set /subcategory:{0CCE923A-69AE-11D9-BED3-505054503030} /success:enable /failure:disable
+auditpol /set /subcategory:{0CCE923A-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Security Group Management
 auditpol /set /subcategory:{0CCE9237-69AE-11D9-BED3-505054503030} /success:enable /failure:disable
 :::: User Account Management
@@ -33,7 +37,7 @@ auditpol /set /subcategory:{0CCE9235-69AE-11D9-BED3-505054503030} /success:enabl
 
 :: Detailed Tracking
 :::: Plug and Play
-:: auditpol /set /subcategory:{0cce9248-69ae-11d9-bed3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0cce9248-69ae-11d9-bed3-505054503030} /success:enable /failure:enable
 :::: Process Creation
 auditpol /set /subcategory:{0CCE922B-69AE-11D9-BED3-505054503030} /success:enable /failure:disable
 :::: Enable command line auditing (Detailed Tracking)
@@ -43,13 +47,13 @@ auditpol /set /subcategory:{0CCE922C-69AE-11D9-BED3-505054503030} /success:enabl
 :::: RPC Events
 :: auditpol /set /subcategory:{0CCE922E-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Audit Token Right Adjustments (default: disabled)
-:: auditpol /set /subcategory:{0CCE924A-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE924A-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 
 :: DS Access
 :::: Directory Service Access (disable for clients)
-:: auditpol /set /subcategory:{0CCE923B-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE923B-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Directory Service Changes (disable for clients)
-:: auditpol /set /subcategory:{0CCE923C-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE923C-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 
 :: Logon/Logoff
 :::: Account Lockout
@@ -71,17 +75,17 @@ auditpol /set /subcategory:{0CCE921B-69AE-11D9-BED3-505054503030} /success:enabl
 :::: Application Generated (currently disabled while testing)
 :: auditpol /set /subcategory:{0CCE9222-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Certification Services (disable for client OSes)
-:: auditpol /set /subcategory:{0CCE9221-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE9221-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Detailed File Share (disabled due to noise)
-:: auditpol /set /subcategory:{0CCE9244-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE9244-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: File Share (disable if too noisy)
 auditpol /set /subcategory:{0CCE9224-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: File System (disabled due to noise)
 auditpol /set /subcategory:{0CCE921D-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Filtering Platform Connection (disable if too noisy)
-:: auditpol /set /subcategory:{0CCE9226-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE9226-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Filtering Platform Packet Drop (disabled due to noise)
-:: auditpol /set /subcategory:{0CCE9225-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE9225-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Kernel Object (disabled due to noise)
 auditpol /set /subcategory:{0CCE921F-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Other Object Access Events
@@ -91,13 +95,13 @@ auditpol /set /subcategory:{0CCE921E-69AE-11D9-BED3-505054503030} /success:enabl
 :::: Removable Storage
 :: auditpol /set /subcategory:{0CCE9245-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: SAM
-:: auditpol /set /subcategory:{0CCE9220-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE9220-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 
 :: Policy Change
 :::: Audit Policy Change
 auditpol /set /subcategory:{0CCE922F-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Authentication Policy Change
-:: auditpol /set /subcategory:{0CCE9230-69AE-11D9-BED3-505054503030} /success:enable /failure:disable
+auditpol /set /subcategory:{0CCE9230-69AE-11D9-BED3-505054503030} /success:enable /failure:disable
 :::: Authorization Policy Change (currently disabled while testing)
 :: auditpol /set /subcategory:{0CCE9231-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Filtering Platform Policy Change (currently disabled while testing)
@@ -109,16 +113,16 @@ auditpol /set /subcategory:{0CCE9234-69AE-11D9-BED3-505054503030} /success:enabl
 
 :: Privilege Use
 :::: Sensitive Privilege Use (disable if too noisy)
-:: auditpol /set /subcategory:{0CCE9228-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE9228-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 
 :: System
 :::: IPsec Driver
-:: auditpol /set /subcategory:{0CCE9213-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE9213-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Other System Events (needs testing)
 :: auditpol /set /subcategory:{0CCE9214-69AE-11D9-BED3-505054503030} /success:disable /failure:enable
 :::: Security State Change
-:: auditpol /set /subcategory:{0CCE9210-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE9210-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: Security System Extension
-:: auditpol /set /subcategory:{0CCE9211-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
+auditpol /set /subcategory:{0CCE9211-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
 :::: System Integrity
 auditpol /set /subcategory:{0CCE9212-69AE-11D9-BED3-505054503030} /success:enable /failure:enable
