1bac84c0e93c2a0b1f37c59ee65518ad5bd95077
smashtitle <smashtitle@fastmail.com>
2025-07-04 15:55:06 -0700

    Update ASD-Servers.bat

--- bat/ASD-Servers.bat
+++ bat/ASD-Servers.bat
@@ -6,12 +6,9 @@ wevtutil set-log "Microsoft-Windows-PowerShell/Operational" /enabled:true
 :: Enable PowerShell Module logging
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging /v EnableModuleLogging /t REG_DWORD /d 1 /f
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ModuleLogging\ModuleNames /v 1 /t REG_SZ /d * /f
-reg add HKLM\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows\PowerShell\ModuleLogging /v EnableModuleLogging /t REG_DWORD /d 1 /f
-reg add HKLM\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows\PowerShell\ModuleLogging\ModuleNames /v 1 /t REG_SZ /d * /f
 
 :: Enable PowerShell Script Block logging
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging /v EnableScriptBlockLogging /t REG_DWORD /d 1 /f
-reg add HKLM\SOFTWARE\WOW6432Node\Policies\Microsoft\Windows\PowerShell\ScriptBlockLogging /v EnableScriptBlockLogging /f /t REG_DWORD /d 1
 
 ::
 :: Configure Security log
